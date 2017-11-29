29a99174bf32fd1b54da24c5ab7da411e509cd8b
Unknown <rpruchitpatel14@gmail.com>
2017-11-29 13:05:48 +0530

    The first and only commit!

---
+++ Assets/Movements.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour {
+
+	public Rigidbody2D starbody;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		starbody.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * speed, Input.GetAxisRaw ("Vertical") * speed);
+	}
+}
---
+++ Assets/finish.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finish : MonoBehaviour {
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "star") {
+			Destroy (col.gameObject);
+		}
+	}
+
+}
